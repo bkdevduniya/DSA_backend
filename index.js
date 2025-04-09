6f72c3cdadb95e846e67aef180f5d38e06997bec
bkdevduniya <bhoodev6232@gmail.com>
2025-04-09 13:49:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use("/app", appRouter);
 app.use("/api", noChache, apiRouter);
 
 // MongoDB connection
-connectDB("mongodb://127.0.0.1:27017/DSA");
+connectDB("mongodb://bkmandawat06:TgxiUbzgTiAGZAF9@ac-9ossrny-shard-00-00.prtfju5.mongodb.net:27017,ac-9ossrny-shard-00-01.prtfju5.mongodb.net:27017,ac-9ossrny-shard-00-02.prtfju5.mongodb.net:27017/?replicaSet=atlas-12dx2t-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=Cluster0");
 
 // Start the server
 app.listen(5000, () => console.log("Server is running on port 5000"));
