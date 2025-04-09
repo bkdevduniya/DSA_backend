71f2c4c8517f2dfffa5dc2a9a857abd26db4f5be
bkdevduniya <bhoodev6232@gmail.com>
2025-04-09 13:51:54 +0530

    Update mongodb.js

--- connections/mongodb.js
+++ connections/mongodb.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async (url) => {
     try {
+        
         await mongoose.connect(url);
         console.log("MongoDB connected");
     } catch (err) {
