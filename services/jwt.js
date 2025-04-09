836cf005ee7f00851fc0e5230ffc274dc4c5e82b
bkdevduniya <bhoodev6232@gmail.com>
2025-04-09 13:53:23 +0530

    Update jwt.js

--- services/jwt.js
+++ services/jwt.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
-const secret="2@@@#672636112$#$A5q6521752`1qa87435";
-
+const secret = process.env.JWT_SECRET || "2@@@#672636112$#$A5q6521752`1qa87435";
 const generateToken=(user)=>{
     const token=jwt.sign({email:user.email,name:user.username,profliePic:user.profilePic,level:user.level,role:user.role},secret,{expiresIn:'7d'});
     return token;
