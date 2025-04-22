94826035bb5759a9da27e416f6390ab61277bc35
bkdevduniya <bhoodev6232@gmail.com>
2025-04-22 17:16:36 +0530

    Delete routes directory

--- routes/app.js
+++
@@ -1,7 +0,0 @@
-const app = require("../controllers/app");
-const express = require("express");
-const router = express.Router();
-
-router.get("/", app);
-
-module.exports = router;
--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const {signup,login}=require('../controllers/auth');
-const express=require('express');
-const router=express.Router();
-
-router.route("/signup").post(signup);
-router.route("/login").post(login);
-
-module.exports=router;
